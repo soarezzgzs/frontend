f2195231810fffb94b8dea231c2f40e5100205cb
Leonardo Soares <leonardo.soares3345@gmail.com>
2026-04-30 21:19:34 -0300

    ajustando

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://backend-m2bd.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3333'
 });
+
 
 export default api
